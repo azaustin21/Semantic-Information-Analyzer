b7d5c197a45e13452eece44018ca3badbb738a3a
azaustin21 <@GitZarePesh789>
2021-06-20 16:37:15 -0500

    Added starting snippet to pull MLK speech to test proof-of-concept.

---
+++ speech_summary.py
@@ -0,0 +1,16 @@
+from collections import Counter
+import re
+import requests
+import bs4
+import nltk
+from nltk.corpus import stopwords
+
+
+def main():
+    url = 'http://www.analytictech.com/mb021/mlk.htm'
+    page = requests.get(url)
+    page.raise_for_status()
+    soup = bs4.BeautifulSoup(page.text, 'html.parser')
+    p_elems = [element.text for element in soup.find_all('p')]
+
+    speech = ''.join(p_elems)
